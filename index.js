c44521f43b4888f6727370abeb1e40d1ddef4995
01Ander <arinconayala@hotmail.com>
2023-03-10 18:16:03 -0500

    ready api rest get

---
+++ index.js
@@ -0,0 +1,49 @@
+const API = 'https://api.escuelajs.co/api/v1';
+const main = document.querySelector('.main')
+
+function fetchData(urlApi) {
+	return fetch(urlApi);
+};
+
+// fetchData(`${API}/products/25`)
+//   .then((response) => response.json())
+//   .then(product => {
+//     const title = document.createElement('h3');
+//     title.innerText = product.title
+//     main.appendChild(title)
+
+//     const img =document.createElement('img');
+//     img.src = product.images[0]
+//     img.width = 250;
+//     main.appendChild(img)
+
+//   })
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log('Finally promise'))
+
+fetchData(`${API}/products?limit=10&offset=5`)
+  .then((response) => response.json())
+  .then(products => {
+    const toRender = [];
+    const section = document.querySelector('.main');
+
+    products.forEach(element => {
+      const art = document.createElement('article');
+      const img = document.createElement('img');
+      const title = document.createElement('p');
+      const price = document.createElement('p')
+
+      img.src = element.images[0];
+      img.width = 250;
+      title.innerText = element.title;
+      price.innerText = `$${element.price}`;
+
+      art.appendChild(img);
+      art.appendChild(title);
+      art.appendChild(price);
+
+      toRender.push(art);
+
+    });
+    section.append(...toRender)
+  })
