55654f3e339d974f0d48d1d8e7276c37f33114d5
01Ander <arinconayala@hotmail.com>
2023-03-13 16:50:22 -0500

    get use axios

--- index.js
+++ index.js
@@ -1,50 +1,12 @@
-const API = 'https://api.escuelajs.co/api/v1';
 const api = axios.create({
   baseURL: 'https://api.escuelajs.co/api/v1'
 });
 
-const main = document.querySelector('.main')
 
-
-// function fetchData(urlApi) {
-//   return fetch(urlApi);
-// };
-
-// async function getProducts() {
-//   const { data }  = await api('/products?limit=12&offset=5')
-//   console.log(data);
-//   const toRender = [];
-//   const section = document.querySelector('.main');
-
-//   data.forEach(element => {
-//     const art = document.createElement('article');
-//     const img = document.createElement('img');
-//     const title = document.createElement('p');
-//     const price = document.createElement('p')
-
-//     art.classList.add(element.category.name);
-
-//     img.src = element.images[0];
-//     title.innerText = element.title;
-//     price.innerText = `$${element.price}`;
-
-//     art.appendChild(img);
-//     art.appendChild(title);
-//     art.appendChild(price);
-
-//     toRender.push(art);
-
-//   });
-//   section.append(...toRender)
-// }
-
-// getProducts();
-
-axios
+api
   .get(`/products?limit=12&offset=5`)
   .then(products => {
     data = products.data
-    console.log(data);
 
     const toRender = [];
     const section = document.querySelector('.main');
@@ -71,19 +33,21 @@ axios
     section.append(...toRender)
   })
 
-  // axios.get('/categories?limit=5&offset=0')
-  // .then(products => {
-  //   console.log(products);
-  //   const toRender = [];
-  //   const list = document.querySelector('.list');
+api
+  .get('/categories?limit=5&offset=0')
+  .then(products => {
+    data = products.data
+    console.log(data);
+    const toRender = [];
+    const list = document.querySelector('.list');
 
-  //   products.forEach(element => {
-  //     const name = document.createElement('li');
-  //     name.innerText = element.name;
-  //     name.classList.add(`li-${element.name}`)
+    data.forEach(element => {
+      const name = document.createElement('li');
+      name.innerText = element.name;
+      name.classList.add(`li-${element.name}`)
 
-  //     toRender.push(name)
-  //   })
-  //   list.append(...toRender)
-  // })
+      toRender.push(name)
+    })
+    list.append(...toRender)
+  })
 
